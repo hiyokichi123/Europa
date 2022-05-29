cc642a1f4ebe15ed4ec53ace68ea7099eb19001d
hiyokichi123 <csyu20085@g.nihon-u.ac.jp>
2022-05-30 02:47:14 +0900

    ver1

---
+++ Assets/Soviet Union/Skybox.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Skybox : UdonSharpBehaviour
+{
+    [Range(0.003f, 0.1f)]
+    public float rotateSpeed;
+
+    public Material sky;
+
+    float rotationRepeatValue;
+    void Update()
+    {
+        rotationRepeatValue = Mathf.Repeat(sky.GetFloat("_Rotation") + rotateSpeed, 360f);
+
+        sky.SetFloat("_Rotation", rotationRepeatValue);
+
+
+    }
+}
---
+++ Assets/Soviet Union/revolution.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class revolution : UdonSharpBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.01f, 0, 0));
+    }
+}
